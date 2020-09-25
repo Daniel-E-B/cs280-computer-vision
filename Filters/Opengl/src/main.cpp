87d23e14d2a488865e8f977cc729a4442ef5ca07
Daniel B <danbern@nuevaschool.org>
2020-09-24 20:10:22 -0700

    2 shaders in a row working...

--- Filters/Opengl/src/main.cpp
+++ Filters/Opengl/src/main.cpp
@@ -19,6 +19,7 @@
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+unsigned int loadTexture(const char *path);
 
 int main()
 {
@@ -46,7 +47,16 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     Shader verticalShader("src/shader.vs", "src/vshader.fs");
-    Shader horizontalShader("src/hshader.vs", "src/hshader.fs");
+    Shader horizontalShader("src/shader.vs", "src/hshader.fs");
+    Shader screenShader("src/screen.vs", "src/screen.fs");
+
+    unsigned int texture = loadTexture("../draken.jpg");
+
+    horizontalShader.use();
+    horizontalShader.setInt("ourTexture", 0);
+
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
 
     float texW = 1200.0 / WIDTH;
     float texH = 800.0 / HEIGHT;
@@ -64,6 +74,16 @@ int main()
         1, 2, 3  // second triangle
     };
 
+    float quadVertices[] = {// vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+                            // positions   // texCoords
+                            -1.0f, 1.0f, 0.0f, 1.0f,
+                            -1.0f, -1.0f, 0.0f, 0.0f,
+                            1.0f, -1.0f, 1.0f, 0.0f,
+
+                            -1.0f, 1.0f, 0.0f, 1.0f,
+                            1.0f, -1.0f, 1.0f, 0.0f,
+                            1.0f, 1.0f, 1.0f, 1.0f};
+
     unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -72,7 +92,7 @@ int main()
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW); // might want to change to dynamic draw?
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // might want to change to dynamic draw?
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
@@ -87,28 +107,42 @@ int main()
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float))); // index, size, type, normalized, stride size, offset of first component
     glEnableVertexAttribArray(2);
 
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // screen quad VAO
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
 
+    unsigned int framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    // create a color attachment texture
+    unsigned int textureColorbuffer;
+    glGenTextures(1, &textureColorbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // clamp to edge is like edge padding
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
 
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("../draken.jpg", &width, &height, &nrChannels, 0);
-
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
+    // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, WIDTH, HEIGHT);                   // use a single renderbuffer object for both a depth AND stencil buffer.
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     // glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -116,8 +150,6 @@ int main()
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 
     // glBindVertexArray(0);
-
-
 
     float t = 0.;
     uint32_t count = 0;
@@ -128,65 +160,87 @@ int main()
     {
         processInput(window);
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+        glClearColor(0.2f, 1.0f, 1.0f, 1.0f); // we see this
         glClear(GL_COLOR_BUFFER_BIT);
 
-        auto start = std::chrono::system_clock::now();
-
-
-        glBindTexture(GL_TEXTURE_2D, texture);
-
-        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        pos.x += velocity * glm::cos(theta);
-        pos.y += velocity * glm::sin(theta);
-        if (pos.y > 1)
-        {
-            pos.y = -1;
-        }
-        else if (pos.y < -1)
-        {
-            pos.y = 1;
-        }
-        if (pos.x > 1)
-        {
-            pos.x = -1;
-        }
-        else if (pos.x < -1)
-        {
-            pos.x = 1;
-        }
-        // theta += 0.001;
-        transform = glm::translate(transform, pos);
-        transform = glm::rotate(transform, theta, glm::vec3(1.0f, 1.0f, 0.0f));
-
-        // get matrix's uniform location and set matrix
-        horizontalShader.use();
-        verticalShader.use();
-        unsigned int transformLoc = glGetUniformLocation(horizontalShader.ID, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-
+        horizontalShader.use(); // we see this
         glBindVertexArray(VAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        vertices[0] += 0.01;
+
+
+        
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // we never see this
+        glClear(GL_COLOR_BUFFER_BIT);
+        // verticalShader.use();
+        screenShader.use();
+        glBindVertexArray(quadVAO);
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer); // if i do texture here and element draw, it works.
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // auto start = std::chrono::system_clock::now();
+
+        // glBindTexture(GL_TEXTURE_2D, texture);
+
+        // glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        // pos.x += velocity * glm::cos(theta);
+        // pos.y += velocity * glm::sin(theta);
+        // if (pos.y > 1)
+        // {
+        //     pos.y = -1;
+        // }
+        // else if (pos.y < -1)
+        // {
+        //     pos.y = 1;
+        // }
+        // if (pos.x > 1)
+        // {
+        //     pos.x = -1;
+        // }
+        // else if (pos.x < -1)
+        // {
+        //     pos.x = 1;
+        // }
+        // // theta += 0.001;
+        // transform = glm::translate(transform, pos);
+        // transform = glm::rotate(transform, theta, glm::vec3(1.0f, 1.0f, 0.0f));
+
+        // // get matrix's uniform location and set matrix
+        // horizontalShader.use();
+        // verticalShader.use();
+        // unsigned int transformLoc = glGetUniformLocation(horizontalShader.ID, "transform");
+        // glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        // glBindVertexArray(VAO);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // vertices[0] += 0.01;
 
         glfwSwapBuffers(window);
 
-        auto end = std::chrono::system_clock::now();
-        std::chrono::duration<double> duration = end - start;
-        ++count;
-        t += duration.count();
-        if (count == 1000)
-        {
-            count = 0;
-            std::cout << "compute + draw time for last 1000 frames: " << t / 1000. << std::endl; // this seems really inaccurate -- I guess it only measure cpu time not gpu
+        // auto end = std::chrono::system_clock::now();
+        // std::chrono::duration<double> duration = end - start;
+        // ++count;
+        // t += duration.count();
+        // if (count == 1000)
+        // {
+        //     count = 0;
+        //     std::cout << "avg compute + draw time for last 1000 frames: " << t / 1000. << std::endl; // this seems really inaccurate -- I guess it only measure cpu time not gpu
 
-            t = 0;
-        }
+        //     t = 0;
+        // }
 
         glfwPollEvents();
     }
 
     glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &quadVAO);
+    glDeleteBuffers(1, &quadVBO);
     glDeleteBuffers(1, &VBO);
     // glDeleteBuffers(1, &EBO);
     glfwTerminate();
@@ -202,4 +256,43 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+// utility function for loading a 2D texture from file
+// ---------------------------------------------------
+unsigned int loadTexture(char const *path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // clamp to edge is like edge padding
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
 }
